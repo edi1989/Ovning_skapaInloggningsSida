7624aaf73d6a43cf2ccdf993b835b376e747d351
Edi Sogorovic <soed19bz@student.ju.segit config --global user.email soed19bz@student.ju.se>
2019-09-13 21:36:07 +0200

    Added comments to all steps made in the script

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+// ****** Start variabler ****** //
+
 var username = "test" //Specifikt användarnamn
 var password = "1234" //Specifikt lösenord
 
 var userelement = document.getElementById("username") // Hämtar inputfältet för username
 var passwordelement = document.getElementById("password") // Hämtar inputfältet för password
 
-var loggainsection = document.getElementById("loggainsection") // Variabel för att hämta inloggningssidan med inmatningsfält
-var loggedin = document.getElementById("loggedin") // Variabel för att hämta sidan vid rätt lösenord
-var failed = document.getElementById("failed") // Variabel för att hämta sidan vid fel lösenord
+var loggainsection = document.getElementById("loggainsection") // Variabel för att hämta inloggningssidan "loggainsection"
+var loggedin = document.getElementById("loggedin") // Variabel för att hämta sidan "loggedin" vid rätt lösenord
+var failed = document.getElementById("failed") // Variabel för att hämta sidan "failed" vid fel lösenord
 
 var userinloggad = localStorage.getItem("inloggad"); // Hämta localstorage information
 
+// ****** Slut variabler ****** //
+
+// ****** Start IF funktion om inloggad/om inte inloggad ****** //
+
 if (userinloggad) {
-    loggainsection.style.display = "none"; //Om inloggad, gömmer sidan med inmatningsfält
-    failed.style.display = "none"; //Om inloggad, gömmer fel lösenord sidan
-    loggedin.style.display = "block"; //Om inloggad, visar välkommen-sidan
+    loggainsection.style.display = "none"; //Om inloggad, göm sidan med inmatningsfält
+    failed.style.display = "none"; //Om inloggad, göm fel lösenord sidan
+    loggedin.style.display = "block"; //Om inloggad, visa välkommen-sidan
 } else {
-    loggainsection.style.display = "block"; //Om ej inloggad, visar startsidan sidan med inmatningsfält
-    failed.style.display = "none"; //Om ej inloggad, gömmer fel lösenord sidan
-    loggedin.style.display = "none"; //Om ej inloggad, gömmer välkommen-sidan om rätt anvnamn och lösenord
+    loggainsection.style.display = "block"; //Om ej inloggad, visa startsidan sidan med inmatningsfält
+    failed.style.display = "none"; //Om ej inloggad, göm fel lösenord sidan
+    loggedin.style.display = "none"; //Om ej inloggad, göm välkommen-sidan om rätt anvnamn och lösenord
 }
+// ****** Slut IF funktion om inloggad/om inte inloggad ****** //
 
 // ****** Start funktion för logga ut ****** //
 
@@ -38,7 +45,7 @@ function login() {
 
     if(userelement.value === username && passwordelement.value === password) //IF funktion för att kontrollera att username = test samt password = 1234 är korrekt
     {
-        localStorage.setItem("inloggad", true); //Sparar localStorage information i webbläsaren
+        localStorage.setItem("inloggad", true); //Sparar localStorage information
         loggedin.style.display = "block"; //Om rätt användaruppgift och lösenord, visar välkommen-sidan
     }
      else
@@ -47,4 +54,3 @@ function login() {
     }
 }
 // ****** Slut funktion för logga in ****** //
-
